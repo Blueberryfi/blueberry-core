426b70ed66a0452099386bb1667be561571f0d0b
gmspacex <gmspacex@gmail.com>
2022-08-10 23:33:08 +1000

    feat: add initial deployment scripts

---
+++ scripts/deploy/univ2oracle.deploy.ts
@@ -0,0 +1,12 @@
+import { ethers } from 'hardhat';
+
+async function main(): Promise<void> {
+
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error: Error) => {
+		console.error(error);
+		process.exit(1);
+	});
