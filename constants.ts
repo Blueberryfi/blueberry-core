1551bb72f4487aef9a2a4648b835e6ef87d7189d
gmspacex <gmspacex@gmail.com>
2022-08-11 11:44:44 +1000

    feat: add uni v2 oracle deploy script

--- constants.ts
+++ constants.ts
@@ -14,6 +14,7 @@ export enum CONTRACT_NAMES {
 	ProxyOracle = "ProxyOracle",
 	UniswapV2Oracle = "UniswapV2Oracle",
 	BalancerPairOracle = "BalancerPairOracle",
+	ERC20KP3ROracle = "ERC20KP3ROracle",
 
 	// Uniswap
 	MockUniswapV2Factory = "MockUniswapV2Factory",
@@ -24,4 +25,11 @@ export enum CONTRACT_NAMES {
 	SafeBox = "SafeBox",
 	SafeBoxETH = "SafeBoxETH",
 	UniswapV2SpellV1 = "UniswapV2SpellV1",
+}
+
+export const ADDRESS = {
+	Keep3rV1Oracle: '0x73353801921417F465377c8d898c6f4C0270282C',
+	USDT: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+	USDC: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+	UNI_V2_USDT_USDC: '0x3041cbd36888becc7bbcbc0045e3b1f144466f5f'
 }
