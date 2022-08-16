e8399579280c89ecdda3d0fa0c7242ca6ce9fc84
gmspacex <gmspacex@gmail.com>
2022-08-16 11:48:43 +1000

    feat: update constants of balancer spell test

--- test/spell/balancer.spell.test.ts
+++ test/spell/balancer.spell.test.ts
@@ -15,17 +15,17 @@ import {
 import { solidity } from 'ethereum-waffle'
 import { near } from '../assertions/near'
 import { roughlyNear } from '../assertions/roughlyNear'
-import { CONTRACT_NAMES } from '../../constants';
+import { ADDRESS, CONTRACT_NAMES } from '../../constants';
 
 chai.use(solidity)
 chai.use(near)
 chai.use(roughlyNear)
 
-const COMPTROLLER = '0x3d5BC3c8d13dcB8bF317092d84783c2697AE9258';	// Cream Finance / Comptroller
-const ICETHER = '0xD06527D5e56A3495252A528C4987003b712860eE';			// Cream Finance / crEth
-const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
-const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
-const BALANCER_LP = '0x8b6e6e7b5b3801fed2cafd4b22b8a16c2f2db21a';
+const COMPTROLLER = ADDRESS.CREAM_COMP;	// Cream Finance / Comptroller
+const ICETHER = ADDRESS.crETH;			// Cream Finance / crEth
+const DAI = ADDRESS.DAI;
+const WETH = ADDRESS.WETH;
+const BALANCER_LP = ADDRESS.BAL_WETH_DAI_8020;
 
 describe('Balancer Spell', () => {
 	let admin: SignerWithAddress;
