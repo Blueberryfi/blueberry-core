ae4c25a61522a370f4b4bf8b7f937a9c32691242
gmspacex <gmspacex@gmail.com>
2022-09-15 19:52:47 +1000

    feat: fix uni v2 oracle decimal issues

--- contracts/oracle/UniswapV2Oracle.sol
+++ contracts/oracle/UniswapV2Oracle.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.9;
 
+import '@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol';
+
 import './UsingBaseOracle.sol';
 import '../utils/BBMath.sol';
 import '../interfaces/IBaseOracle.sol';
@@ -19,16 +21,18 @@ contract UniswapV2Oracle is UsingBaseOracle, IBaseOracle {
         address token1 = IUniswapV2Pair(pair).token1();
         uint256 totalSupply = IUniswapV2Pair(pair).totalSupply();
         (uint256 r0, uint256 r1, ) = IUniswapV2Pair(pair).getReserves();
-        uint256 sqrtK = BBMath.sqrt(r0 * r1).fdiv(totalSupply); // in 2**112
+
         uint256 px0 = base.getETHPx(token0); // in 2**112
         uint256 px1 = base.getETHPx(token1); // in 2**112
-        // fair token0 amt: sqrtK * sqrt(px1/px0)
-        // fair token1 amt: sqrtK * sqrt(px0/px1)
-        // fair lp price = 2 * sqrt(px0 * px1)
-        // split into 2 sqrts multiplication to prevent uint overflow (note the 2**112)
-        return
-            (sqrtK * 2) *
-            (BBMath.sqrt(px0) / 2**56) *
-            (BBMath.sqrt(px1) / 2**56);
+
+        uint256 t0Decimal = IERC20Metadata(token0).decimals();
+        uint256 t1Decimal = IERC20Metadata(token1).decimals();
+
+        uint256 totalReserve = (r0 * px0) /
+            10**t0Decimal +
+            (r1 * px1) /
+            10**t1Decimal;
+
+        return (totalReserve * 10**18) / totalSupply;
     }
 }
--- test/oracle/uniswap-lp-oracle.test.ts
+++ test/oracle/uniswap-lp-oracle.test.ts
@@ -1,101 +1,60 @@
-import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import chai, { expect } from 'chai';
 import { BigNumber } from 'ethers';
 import { ethers } from 'hardhat';
 import { ADDRESS, CONTRACT_NAMES } from "../../constants"
 import {
-  SimpleOracle,
   UniswapV2Oracle,
   IERC20Ex,
   IUniswapV2Pair,
+  ChainlinkAdapterOracle,
 } from '../../typechain-types';
-import { solidity } from 'ethereum-waffle'
-import { near } from '../assertions/near'
+import UniPairABI from '../../abi/IUniswapV2Pair.json';
 import { roughlyNear } from '../assertions/roughlyNear'
 
-chai.use(solidity)
-chai.use(near)
 chai.use(roughlyNear)
 
+const OneDay = 86400;
+
 describe('Uniswap LP Oracle', () => {
-  let simpleOracle: SimpleOracle;
   let uniswapOracle: UniswapV2Oracle;
+  let chainlinkAdapterOracle: ChainlinkAdapterOracle;
   before(async () => {
-    const SimpleOracleFactory = await ethers.getContractFactory(CONTRACT_NAMES.SimpleOracle);
-    simpleOracle = <SimpleOracle>await SimpleOracleFactory.deploy();
-    await simpleOracle.deployed();
+    const ChainlinkAdapterOracle = await ethers.getContractFactory(CONTRACT_NAMES.ChainlinkAdapterOracle);
+    chainlinkAdapterOracle = <ChainlinkAdapterOracle>await ChainlinkAdapterOracle.deploy();
+    await chainlinkAdapterOracle.deployed();
+
+    await chainlinkAdapterOracle.setMaxDelayTimes(
+      [ADDRESS.USDC, ADDRESS.CRV],
+      [OneDay, OneDay]
+    );
 
     const UniswapOracleFactory = await ethers.getContractFactory(CONTRACT_NAMES.UniswapV2Oracle);
-    uniswapOracle = <UniswapV2Oracle>await UniswapOracleFactory.deploy(simpleOracle.address);
+    uniswapOracle = <UniswapV2Oracle>await UniswapOracleFactory.deploy(chainlinkAdapterOracle.address);
     await uniswapOracle.deployed();
   })
 
-  describe('Basic', async () => {
-    it('get LP supply', async () => {
-      const daiAddr = ADDRESS.DAI;
-      const usdcAddr = ADDRESS.USDC;
-      const usdtAddr = ADDRESS.USDT;
-      const wethAddr = ADDRESS.WETH;
-      const eth_usdtAddr = ADDRESS.UNI_V2_USDT_WETH;
-      const eth_usdcAddr = ADDRESS.UNI_V2_USDC_WETH;
-      const usdt_usdcAddr = ADDRESS.UNI_V2_USDT_USDC;
-      const uniPairAddr = ADDRESS.UNI_V2_DAI_WETH;
+  it("USDC/CRV LP Price", async () => {
+    const pair = <IUniswapV2Pair>await ethers.getContractAt(UniPairABI, ADDRESS.UNI_V2_USDC_CRV);
+    const oraclePrice = await uniswapOracle.getETHPx(ADDRESS.UNI_V2_USDC_CRV);
 
-      const weth = <IERC20Ex>await ethers.getContractAt(CONTRACT_NAMES.IERC20Ex, wethAddr);
-      const usdt = <IERC20Ex>await ethers.getContractAt(CONTRACT_NAMES.IERC20Ex, usdtAddr);
-      const eth_usdt = <IERC20Ex>await ethers.getContractAt(CONTRACT_NAMES.IERC20Ex, eth_usdtAddr);
-      const eth_usdc = <IERC20Ex>await ethers.getContractAt(CONTRACT_NAMES.IERC20Ex, eth_usdcAddr);
-      const usdt_usdc = <IERC20Ex>await ethers.getContractAt(CONTRACT_NAMES.IERC20Ex, usdt_usdcAddr);
-      const uniPair = <IUniswapV2Pair>await ethers.getContractAt(CONTRACT_NAMES.IUniswapV2Pair, uniPairAddr);
+    // Calculate real lp price manually
+    const { reserve0, reserve1 } = await pair.getReserves();
+    const totalSupply = await pair.totalSupply();
+    const token0 = await pair.token0();
+    const token1 = await pair.token1();
+    const token0Price = await chainlinkAdapterOracle.getETHPx(token0);
+    const token1Price = await chainlinkAdapterOracle.getETHPx(token1);
+    const token0Contract = <IERC20Ex>await ethers.getContractAt(CONTRACT_NAMES.IERC20Ex, token0);
+    const token1Contract = <IERC20Ex>await ethers.getContractAt(CONTRACT_NAMES.IERC20Ex, token1);
+    const token0Decimal = await token0Contract.decimals();
+    const token1Decimal = await token1Contract.decimals();
 
-      const reserves = await uniPair.getReserves();
-      const token0 = await uniPair.token0();
+    const token0Amount = token0Price.mul(reserve0)
+      .div(BigNumber.from(10).pow(token0Decimal));
+    const token1Amount = token1Price.mul(reserve1)
+      .div(BigNumber.from(10).pow(token1Decimal));
+    const price = token0Amount.add(token1Amount).mul(BigNumber.from(10).pow(18)).div(totalSupply);
 
-      let wethPrice = ethers.constants.Zero;
-      if (token0 === wethAddr) {
-        wethPrice = BigNumber.from(10).pow(18).mul(reserves.reserve1).div(reserves.reserve0);
-      } else {
-        wethPrice = BigNumber.from(10).pow(18).mul(reserves.reserve0).div(reserves.reserve1);
-      }
-
-      await simpleOracle.setETHPx(
-        [
-          daiAddr,
-          usdtAddr,
-          usdcAddr,
-          wethAddr
-        ],
-        [
-          BigNumber.from(2).pow(112).mul(BigNumber.from(10).pow(18)).div(wethPrice),
-          BigNumber.from(2).pow(112).mul(BigNumber.from(10).pow(30)).div(wethPrice),
-          BigNumber.from(2).pow(112).mul(BigNumber.from(10).pow(30)).div(wethPrice),
-          BigNumber.from(2).pow(112)
-        ]
-      );
-
-      const eth_usdt_lp = await uniswapOracle.getETHPx(eth_usdtAddr);
-      const eth_usdc_lp = await uniswapOracle.getETHPx(eth_usdcAddr);
-      const usdt_usdc_lp = await uniswapOracle.getETHPx(usdt_usdcAddr);
-
-      const eth_usdt_balance = await weth.balanceOf(eth_usdtAddr);
-      const eth_usdt_supply = await eth_usdt.totalSupply();
-
-      const eth_usdc_balance = await weth.balanceOf(eth_usdcAddr);
-      const eth_usdc_supply = await eth_usdc.totalSupply();
-
-      const usdt_usdc_balance = await usdt.balanceOf(usdt_usdcAddr);
-      const px = await simpleOracle.getETHPx(usdtAddr);
-      const usdt_usdc_supply = await usdt_usdc.totalSupply();
-
-      expect(eth_usdt_lp).to.be.roughlyNear(
-        eth_usdt_balance.mul(2).mul(BigNumber.from(2).pow(112)).div(eth_usdt_supply)
-      );
-      expect(eth_usdc_lp).to.be.roughlyNear(
-        eth_usdc_balance.mul(2).mul(BigNumber.from(2).pow(112)).div(eth_usdc_supply)
-      );
-      expect(usdt_usdc_lp).to.be.roughlyNear(
-        usdt_usdc_balance.mul(2).mul(px).div(usdt_usdc_supply)
-      );
-    })
-  });
+    expect(price).to.be.equal(oraclePrice);
+  })
 });
