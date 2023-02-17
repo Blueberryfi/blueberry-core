06f5ea98dc4b0b80b411a28cb7d75cc0766a6c67
blueChamel <mauro@blueberry.garden>
2023-02-17 06:00:11 -0800

    fix: audit-24

--- contracts/oracle/UniswapV3AdapterOracle.sol
+++ contracts/oracle/UniswapV3AdapterOracle.sol
@@ -46,10 +46,10 @@ contract UniswapV3AdapterOracle is IBaseOracle, UsingBaseOracle, BaseAdapter {
         address stablePool = stablePools[token];
         if (stablePool == address(0)) revert NO_STABLEPOOL(token);
 
-        address token0 = IUniswapV3Pool(stablePool).token0();
-        address token1 = IUniswapV3Pool(stablePool).token1();
-        token1 = token0 == token ? token1 : token0; // get stable token address
-        uint256 stableDecimals = uint256(IERC20Metadata(token1).decimals());
+        address fLiqToken = IUniswapV3Pool(stablePool).token0();
+        address sLiqToken = IUniswapV3Pool(stablePool).token1();
+        sLiqToken = fLiqToken == token ? sLiqToken : fLiqToken; // get stable token address
+        uint256 stableDecimals = uint256(IERC20Metadata(sLiqToken).decimals());
         uint256 tokenDecimals = uint256(IERC20Metadata(token).decimals());
         (int24 arithmeticMeanTick, ) = UniV3WrappedLibMockup.consult(
             stablePool,
@@ -60,11 +60,11 @@ contract UniswapV3AdapterOracle is IBaseOracle, UsingBaseOracle, BaseAdapter {
                 arithmeticMeanTick,
                 uint128(10**tokenDecimals),
                 token,
-                token1
+                sLiqToken
             );
 
         return
-            (quoteTokenAmountForStable * base.getPrice(token1)) /
+            (quoteTokenAmountForStable * base.getPrice(sLiqToken)) /
             10**stableDecimals;
     }
 }
