da7c1256b7b87bace8e3f5a8bb533bdeafe90785
blueChamel <mauro@blueberry.garden>
2023-02-17 05:49:49 -0800

    fix: audit-26

--- contracts/oracle/CoreOracle.sol
+++ contracts/oracle/CoreOracle.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.16;
-pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/IERC20MetadataUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
