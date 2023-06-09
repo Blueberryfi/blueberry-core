dfe0479653f01453f468e58012bd0462f47eeed5
bwafflef <optisman92@gmail.com>
2023-06-09 09:48:54 +0900

    feat: implement aura pending reward calculation logic in WAuraPools

---
+++ contracts/interfaces/aura/IAura.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.16;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/IERC20MetadataUpgradeable.sol";
+
+interface IAura is IERC20MetadataUpgradeable {
+    function INIT_MINT_AMOUNT() external view returns (uint256);
+
+    function reductionPerCliff() external view returns (uint256);
+
+    function totalCliffs() external view returns (uint256);
+
+    function EMISSIONS_MAX_SUPPLY() external view returns (uint256);
+}
