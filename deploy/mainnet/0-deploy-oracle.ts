a096579e94a6567173eac898b4cf107e69bb99e9
0xm00neth <0xm00neth@gmail.com>
2023-07-19 07:38:19 -0500

    update oracle deployment scripts

--- deploy/mainnet/0-deploy-oracle.ts
+++ deploy/mainnet/0-deploy-oracle.ts
@@ -1,8 +1,14 @@
-import { BigNumber } from 'ethers';
-import fs from 'fs';
+import { BigNumber } from "ethers";
+import fs from "fs";
 import { ethers, network } from "hardhat";
-import { ADDRESS } from '../../constant';
-import { AggregatorOracle, BandAdapterOracle, ChainlinkAdapterOracle, CoreOracle, UniswapV3AdapterOracle } from '../../typechain-types';
+import { ADDRESS } from "../../constant";
+import {
+  AggregatorOracle,
+  BandAdapterOracle,
+  ChainlinkAdapterOracle,
+  CoreOracle,
+  UniswapV3AdapterOracle,
+} from "../../typechain-types";
 
 const deploymentPath = "./deployments";
 const deploymentFilePath = `${deploymentPath}/${network.name}.json`;
@@ -23,73 +29,112 @@ async function main(): Promise<void> {
   console.log("Deployer:", deployer.address);
 
   // Band Adapter Oracle
-  const BandAdapterOracle = await ethers.getContractFactory("BandAdapterOracle");
-  const bandOracle = <BandAdapterOracle>await BandAdapterOracle.deploy(ADDRESS.BandStdRef);
+  const BandAdapterOracle = await ethers.getContractFactory(
+    "BandAdapterOracle"
+  );
+  const bandOracle = <BandAdapterOracle>(
+    await BandAdapterOracle.deploy(ADDRESS.BandStdRef)
+  );
   await bandOracle.deployed();
   console.log("Band Oracle Address:", bandOracle.address);
   deployment.BandAdapterOracle = bandOracle.address;
   writeDeployments(deployment);
 
-  console.log('Setting up Token configs on Band Oracle\nMax Delay Times: 1 day 12 hours');
-  await bandOracle.setTimeGap([
-    ADDRESS.USDC,
-    ADDRESS.DAI,
-    ADDRESS.CRV,
-    ADDRESS.SUSHI,
-    ADDRESS.WBTC,
-    ADDRESS.WETH,
-  ], [
-    129600, 129600, 129600, 129600, 129600, 129600,
-  ]);
-  await bandOracle.setSymbols([
-    ADDRESS.USDC,
-    ADDRESS.DAI,
-    ADDRESS.CRV,
-    ADDRESS.SUSHI,
-    ADDRESS.WBTC,
-    ADDRESS.WETH,
-  ], ['USDC', 'DAI', 'CRV', 'SUSHI', 'WBTC', 'ETH']);
+  console.log(
+    "Setting up Token configs on Band Oracle\nMax Delay Times: 1 day 12 hours"
+  );
+  await bandOracle.setTimeGap(
+    [
+      ADDRESS.USDC,
+      ADDRESS.DAI,
+      ADDRESS.CRV,
+      ADDRESS.MIM,
+      ADDRESS.LINK,
+      ADDRESS.WBTC,
+      ADDRESS.WETH,
+      ADDRESS.OHM,
+      ADDRESS.ALCX,
+      ADDRESS.wstETH,
+      ADDRESS.BAL,
+    ],
+    [
+      129600, 129600, 129600, 129600, 129600, 129600, 129600, 129600, 129600,
+      129600, 129600,
+    ]
+  );
+  await bandOracle.setSymbols(
+    [
+      ADDRESS.USDC,
+      ADDRESS.DAI,
+      ADDRESS.CRV,
+      ADDRESS.MIM,
+      ADDRESS.LINK,
+      ADDRESS.WBTC,
+      ADDRESS.WETH,
+      ADDRESS.OHM,
+      ADDRESS.ALCX,
+      ADDRESS.wstETH,
+      ADDRESS.BAL,
+    ],
+    [
+      "USDC",
+      "DAI",
+      "CRV",
+      "MIM",
+      "LINK",
+      "WBTC",
+      "ETH",
+      "OHM",
+      "ALCX",
+      "wstETH",
+      "BAL",
+    ]
+  );
 
   // Chainlink Adapter Oracle
-  const ChainlinkAdapterOracle = await ethers.getContractFactory("ChainlinkAdapterOracle");
-  const chainlinkOracle = <ChainlinkAdapterOracle>await ChainlinkAdapterOracle.deploy(ADDRESS.ChainlinkRegistry);
+  const ChainlinkAdapterOracle = await ethers.getContractFactory(
+    "ChainlinkAdapterOracle"
+  );
+  const chainlinkOracle = <ChainlinkAdapterOracle>(
+    await ChainlinkAdapterOracle.deploy(ADDRESS.ChainlinkRegistry)
+  );
   await chainlinkOracle.deployed();
-  console.log('Chainlink Oracle Address:', chainlinkOracle.address);
+  console.log("Chainlink Oracle Address:", chainlinkOracle.address);
   deployment.ChainlinkAdapterOracle = chainlinkOracle.address;
   writeDeployments(deployment);
 
-  console.log('Setting up USDC config on Chainlink Oracle\nMax Delay Times: 129900s');
-  await chainlinkOracle.setTimeGap([
-    ADDRESS.USDC,
-    ADDRESS.DAI,
-    ADDRESS.CRV,
-    ADDRESS.SUSHI,
-    ADDRESS.WBTC,
-    ADDRESS.WETH,
-  ], [
-    129600, 129600, 129600, 129600, 129600, 129600,
-  ]);
-  await chainlinkOracle.setTokenRemappings([
-    ADDRESS.USDC,
-    ADDRESS.DAI,
-    ADDRESS.CRV,
-    ADDRESS.SUSHI,
-    ADDRESS.WBTC,
-    ADDRESS.WETH,
-  ], [
-    ADDRESS.USDC,
-    ADDRESS.DAI,
-    ADDRESS.CRV,
-    ADDRESS.SUSHI,
-    ADDRESS.CHAINLINK_BTC,
-    ADDRESS.CHAINLINK_ETH,
-  ]);
+  console.log(
+    "Setting up USDC config on Chainlink Oracle\nMax Delay Times: 129900s"
+  );
+  await chainlinkOracle.setTimeGap(
+    [
+      ADDRESS.USDC,
+      ADDRESS.DAI,
+      ADDRESS.CRV,
+      ADDRESS.MIM,
+      ADDRESS.LINK,
+      ADDRESS.WBTC,
+      ADDRESS.WETH,
+      ADDRESS.OHM,
+      ADDRESS.ALCX,
+      ADDRESS.wstETH,
+      ADDRESS.BAL,
+    ],
+    [
+      129600, 129600, 129600, 129600, 129600, 129600, 129600, 129600, 129600,
+      129600, 129600,
+    ]
+  );
+  await chainlinkOracle.setTokenRemappings(
+    [ADDRESS.WBTC, ADDRESS.WETH],
+    [ADDRESS.CHAINLINK_BTC, ADDRESS.CHAINLINK_ETH]
+  );
 
   // Aggregator Oracle
   const AggregatorOracle = await ethers.getContractFactory("AggregatorOracle");
   const aggregatorOracle = <AggregatorOracle>await AggregatorOracle.deploy();
   await aggregatorOracle.deployed();
-  console.log('Aggregator Oracle Address:', aggregatorOracle.address);
+  console.log("Aggregator Oracle Address:", aggregatorOracle.address);
   deployment.AggregatorOracle = aggregatorOracle.address;
   writeDeployments(deployment);
 
@@ -98,30 +143,52 @@ async function main(): Promise<void> {
       ADDRESS.USDC,
       ADDRESS.DAI,
       ADDRESS.CRV,
-      ADDRESS.SUSHI,
+      ADDRESS.MIM,
+      ADDRESS.LINK,
       ADDRESS.WBTC,
       ADDRESS.WETH,
-    ], [
-    ethers.utils.parseEther("1.05"),
-    ethers.utils.parseEther("1.05"),
-    ethers.utils.parseEther("1.05"),
-    ethers.utils.parseEther("1.05"),
-    ethers.utils.parseEther("1.05"),
-    ethers.utils.parseEther("1.05"),
-  ], [
-    [bandOracle.address, chainlinkOracle.address],
-    [bandOracle.address, chainlinkOracle.address],
-    [bandOracle.address, chainlinkOracle.address],
-    [bandOracle.address, chainlinkOracle.address],
-    [bandOracle.address, chainlinkOracle.address],
-    [bandOracle.address, chainlinkOracle.address],
-  ]);
+      ADDRESS.OHM,
+      ADDRESS.ALCX,
+      ADDRESS.wstETH,
+      ADDRESS.BAL,
+    ],
+    [
+      ethers.utils.parseEther("1.05"),
+      ethers.utils.parseEther("1.05"),
+      ethers.utils.parseEther("1.05"),
+      ethers.utils.parseEther("1.05"),
+      ethers.utils.parseEther("1.05"),
+      ethers.utils.parseEther("1.05"),
+      ethers.utils.parseEther("1.05"),
+      ethers.utils.parseEther("1.05"),
+      ethers.utils.parseEther("1.05"),
+      ethers.utils.parseEther("1.05"),
+      ethers.utils.parseEther("1.05"),
+    ],
+    [
+      [bandOracle.address, chainlinkOracle.address],
+      [bandOracle.address, chainlinkOracle.address],
+      [bandOracle.address, chainlinkOracle.address],
+      [bandOracle.address, chainlinkOracle.address],
+      [bandOracle.address, chainlinkOracle.address],
+      [bandOracle.address, chainlinkOracle.address],
+      [bandOracle.address, chainlinkOracle.address],
+      [bandOracle.address, chainlinkOracle.address],
+      [bandOracle.address, chainlinkOracle.address],
+      [bandOracle.address, chainlinkOracle.address],
+      [bandOracle.address, chainlinkOracle.address],
+    ]
+  );
 
   // Uni V3 Adapter Oracle
-  const UniswapV3AdapterOracle = await ethers.getContractFactory("UniswapV3AdapterOracle");
-  const uniV3Oracle = <UniswapV3AdapterOracle>await UniswapV3AdapterOracle.deploy(aggregatorOracle.address);
+  const UniswapV3AdapterOracle = await ethers.getContractFactory(
+    "UniswapV3AdapterOracle"
+  );
+  const uniV3Oracle = <UniswapV3AdapterOracle>(
+    await UniswapV3AdapterOracle.deploy(aggregatorOracle.address)
+  );
   await uniV3Oracle.deployed();
-  console.log('Uni V3 Oracle Address:', uniV3Oracle.address);
+  console.log("Uni V3 Oracle Address:", uniV3Oracle.address);
   deployment.UniswapV3AdapterOracle = uniV3Oracle.address;
   writeDeployments(deployment);
 
@@ -132,27 +199,40 @@ async function main(): Promise<void> {
   const CoreOracle = await ethers.getContractFactory("CoreOracle");
   const coreOracle = <CoreOracle>await CoreOracle.deploy();
   await coreOracle.deployed();
-  console.log('Core Oracle Address:', coreOracle.address);
+  console.log("Core Oracle Address:", coreOracle.address);
   deployment.CoreOracle = coreOracle.address;
   writeDeployments(deployment);
 
-  await coreOracle.setRoutes([
-    ADDRESS.USDC,
-    ADDRESS.DAI,
-    ADDRESS.CRV,
-    ADDRESS.SUSHI,
-    ADDRESS.WBTC,
-    ADDRESS.WETH,
-    ADDRESS.ICHI
-  ], [
-    aggregatorOracle.address,
-    aggregatorOracle.address,
-    aggregatorOracle.address,
-    aggregatorOracle.address,
-    aggregatorOracle.address,
-    aggregatorOracle.address,
-    uniV3Oracle.address
-  ]);
+  await coreOracle.setRoutes(
+    [
+      ADDRESS.USDC,
+      ADDRESS.DAI,
+      ADDRESS.CRV,
+      ADDRESS.MIM,
+      ADDRESS.LINK,
+      ADDRESS.WBTC,
+      ADDRESS.WETH,
+      ADDRESS.OHM,
+      ADDRESS.ALCX,
+      ADDRESS.wstETH,
+      ADDRESS.BAL,
+      ADDRESS.ICHI,
+    ],
+    [
+      aggregatorOracle.address,
+      aggregatorOracle.address,
+      aggregatorOracle.address,
+      aggregatorOracle.address,
+      aggregatorOracle.address,
+      aggregatorOracle.address,
+      aggregatorOracle.address,
+      aggregatorOracle.address,
+      aggregatorOracle.address,
+      aggregatorOracle.address,
+      aggregatorOracle.address,
+      uniV3Oracle.address,
+    ]
+  );
 }
 
 main()
--- deploy/mainnet/0-mainnet.deploy.ts
+++ deploy/mainnet/0-mainnet.deploy.ts
@@ -1,61 +1,18 @@
-import { BigNumber } from 'ethers';
-import { ethers, upgrades } from 'hardhat';
+import fs from "fs";
+import { ethers, upgrades, network } from 'hardhat';
 import { ADDRESS, CONTRACT_NAMES } from '../../constant';
 import { AggregatorOracle, BandAdapterOracle, BlueBerryBank, ChainlinkAdapterOracle, CoreOracle, IchiVaultOracle, IchiSpell, ProtocolConfig, UniswapV3AdapterOracle, WERC20, WIchiFarm } from '../../typechain-types';
+
+const deploymentPath = "./deployments";
+const deploymentFilePath = `${deploymentPath}/${network.name}.json`;
 
 async function main(): Promise<void> {
 	const [deployer] = await ethers.getSigners();
 
-	// Band Adapter Oracle
-	const BandAdapterOracle = await ethers.getContractFactory(CONTRACT_NAMES.BandAdapterOracle);
-	const bandOracle = <BandAdapterOracle>await BandAdapterOracle.deploy(ADDRESS.BandStdRef);
-	await bandOracle.deployed();
-	console.log("Band Oracle Address:", bandOracle.address);
-
-	console.log('Setting up USDC config on Band Oracle\nMax Delay Times: 11100s, Symbol: USDC');
-	await bandOracle.setTimeGap([ADDRESS.USDC], [11100]);
-	await bandOracle.setSymbols([ADDRESS.USDC], ['USDC']);
-
-	// Chainlink Adapter Oracle
-	const ChainlinkAdapterOracle = await ethers.getContractFactory(CONTRACT_NAMES.ChainlinkAdapterOracle);
-	const chainlinkOracle = <ChainlinkAdapterOracle>await ChainlinkAdapterOracle.deploy(ADDRESS.ChainlinkRegistry);
-	await chainlinkOracle.deployed();
-	console.log('Chainlink Oracle Address:', chainlinkOracle.address);
-
-	console.log('Setting up USDC config on Chainlink Oracle\nMax Delay Times: 129900s');
-	await chainlinkOracle.setTimeGap([ADDRESS.USDC], [129900]);
-
-	// Aggregator Oracle
-	const AggregatorOracle = await ethers.getContractFactory(CONTRACT_NAMES.AggregatorOracle);
-	const aggregatorOracle = <AggregatorOracle>await AggregatorOracle.deploy();
-	await aggregatorOracle.deployed();
-	console.log('Aggregator Oracle Address:', aggregatorOracle.address);
-
-	await aggregatorOracle.setPrimarySources(
-		ADDRESS.USDC,
-		BigNumber.from(10).pow(16).mul(105), // 5%
-		[bandOracle.address, chainlinkOracle.address]
-	);
-
-	// Uni V3 Adapter Oracle
-	const UniswapV3AdapterOracle = await ethers.getContractFactory(CONTRACT_NAMES.UniswapV3AdapterOracle);
-	const uniV3Oracle = <UniswapV3AdapterOracle>await UniswapV3AdapterOracle.deploy(aggregatorOracle.address);
-	await uniV3Oracle.deployed();
-	console.log('Uni V3 Oracle Address:', uniV3Oracle.address);
-
-	await uniV3Oracle.setStablePools([ADDRESS.ICHI], [ADDRESS.UNI_V3_ICHI_USDC]);
-	await uniV3Oracle.setTimeGap([ADDRESS.ICHI], [10]); // 10s ago
-
-	// Core Oracle
-	const CoreOracle = await ethers.getContractFactory(CONTRACT_NAMES.CoreOracle);
-	const coreOracle = <CoreOracle>await CoreOracle.deploy();
-	await coreOracle.deployed();
-	console.log('Core Oracle Address:', coreOracle.address);
-
-	await coreOracle.setRoutes(
-		[ADDRESS.USDC, ADDRESS.ICHI],
-		[aggregatorOracle.address, uniV3Oracle.address]
-	);
+	const deployment = fs.existsSync(deploymentFilePath)
+    ? JSON.parse(fs.readFileSync(deploymentFilePath).toString())
+    : {};
+	const coreOracle = <CoreOracle>await ethers.getContractAt(CONTRACT_NAMES.CoreOracle, deployment.CoreOracle);
 
 	// Ichi Lp Oracle
 	const IchiVaultOracle = await ethers.getContractFactory(CONTRACT_NAMES.IchiVaultOracle);
