1e4174d63e61b3d38c36649dbe2c14c20b601286
baileyspraggins <baileyspraggins@me.com>
2024-01-12 21:44:02 -0600

    Curve Oracles documentation and visability

--- contracts/interfaces/ICurveOracle.sol
+++ contracts/interfaces/ICurveOracle.sol
@@ -2,15 +2,26 @@
 
 pragma solidity 0.8.22;
 
-import "./IBaseOracle.sol";
+import { IBaseOracle } from "./IBaseOracle.sol";
+import { ICurveAddressProvider } from "../interfaces/curve/ICurveAddressProvider.sol";
 
-/// @title ICurveOracle
-/// @notice Interface for the CurveOracle contract which provides price feed data for assets on Curve Finance.
+/**
+ * @title ICurveOracle
+ * @notice Interface for the CurveOracle contract which provides price feed data for assets on Curve Finance.
+ */
 interface ICurveOracle is IBaseOracle {
-    /// @notice Fetches relevant information about a Curve liquidity pool.
-    /// @param crvLp The address of the Curve liquidity pool token (LP token).
-    /// @return pool Address of the pool contract.
-    /// @return coins A list of underlying tokens in the Curve liquidity pool.
-    /// @return virtualPrice The current virtual price of the LP token for the given Curve liquidity pool.
+    /// @dev Emitted when a Curve LP token is registered with its associated pool and underlying tokens.
+    event CurveLpRegistered(address crvLp, address pool, address[] underlyingTokens);
+
+    /**
+     * @notice Fetches relevant information about a Curve liquidity pool.
+     * @param crvLp The address of the Curve liquidity pool token (LP token).
+     * @return pool Address of the pool contract.
+     * @return coins A list of underlying tokens in the Curve liquidity pool.
+     * @return virtualPrice The current virtual price of the LP token for the given Curve liquidity pool.
+     */
     function getPoolInfo(address crvLp) external returns (address pool, address[] memory coins, uint256 virtualPrice);
+
+    /// @notice Returns the Curve Address Provider.
+    function getAddressProvider() external view returns (ICurveAddressProvider);
 }
--- contracts/oracle/CurveVolatileOracle.sol
+++ contracts/oracle/CurveVolatileOracle.sol
@@ -20,9 +20,11 @@ import { IBaseOracle } from "../interfaces/IBaseOracle.sol";
 import { ICurveAddressProvider } from "../interfaces/curve/ICurveAddressProvider.sol";
 import { ICurvePool } from "../interfaces/curve/ICurvePool.sol";
 
-/// @author BlueberryProtocol
-/// @title Curve Volatile Oracle
-/// @notice Oracle contract which privides price feeds for Curve volatile pool LP tokens
+/**
+ * @title CurveVolatileOracle
+ * @author BlueberryProtocol
+ * @notice Oracle contract which privides price feeds for Curve volatile pool LP tokens
+ */
 contract CurveVolatileOracle is CurveBaseOracle {
     using FixedPoint for uint256;
 
@@ -33,9 +35,8 @@ contract CurveVolatileOracle is CurveBaseOracle {
     uint16 private constant _PERCENTAGE_FACTOR = 1e4; /// 100% represented in fixed point format
     uint256 private constant _RANGE_WIDTH = 200; // Represents a 2% range width
 
-    /// @dev The lower bound for the contract's token-to-underlying exchange rate.
-    /// @notice Used to protect against LP token / share price manipulation.
-    mapping(address => uint256) public lowerBound;
+    /// @dev LP Token to lower bound of token-to-underlying exchange rate
+    mapping(address => uint256) private _lowerBound;
 
     /// @dev Event emitted when the bounds for the token-to-underlying exchange rate is changed.
     event NewLimiterParams(uint256 lowerBound, uint256 upperBound);
@@ -44,54 +45,18 @@ contract CurveVolatileOracle is CurveBaseOracle {
                                      CONSTRUCTOR
     //////////////////////////////////////////////////////////////////////////*/
 
-    /// @notice Constructor to set initial values for the Curve Volatile Oracle
-    /// @param base_ Address of the base oracle
-    /// @param addressProvider_ Address of the curve address provider
-    constructor(IBaseOracle base_, ICurveAddressProvider addressProvider_) CurveBaseOracle(base_, addressProvider_) {}
+    /**
+     * @notice Constructor initializes the CurveBaseOracle with the provided parameters.
+     * @param base Address of the base oracle
+     * @param addressProvider Address of the curve address provider
+     */
+    constructor(IBaseOracle base, ICurveAddressProvider addressProvider) CurveBaseOracle(base, addressProvider) {}
 
     /*//////////////////////////////////////////////////////////////////////////
                                       FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*/
 
-    /// @dev Updates the bounds for the exchange rate value
-    /// @param _crvLp The curve lp token
-    /// @param _lowerBound The new lower bound (the upper bound is computed dynamically)
-    ///                    from the lower bound
-    function setLimiter(address _crvLp, uint256 _lowerBound) external onlyOwner {
-        _setLimiter(_crvLp, _lowerBound);
-    }
-
-    /// @dev Internal implementation for setting the limiter
-    function _setLimiter(address _crvLp, uint256 _lowerBound) internal {
-        if (_lowerBound == 0 || !_checkCurrentValueInBounds(_crvLp, _lowerBound, _upperBound(_lowerBound))) {
-            revert Errors.INCORRECT_LIMITS();
-        }
-
-        lowerBound[_crvLp] = _lowerBound;
-        emit NewLimiterParams(_lowerBound, _upperBound(_lowerBound));
-    }
-
-    /// Checks if the current value is within the specified bounds
-    function _checkCurrentValueInBounds(
-        address _crvLp,
-        uint256 _lowerBound,
-        uint256 __upperBound
-    ) internal view returns (bool) {
-        (, , uint256 virtualPrice) = _getPoolInfo(_crvLp);
-        if (virtualPrice < _lowerBound || virtualPrice > __upperBound) {
-            return false;
-        }
-        return true;
-    }
-
-    /// Override function to check for reentrancy
-    function _checkReentrant(address _pool, uint256) internal override {
-        ICurvePool pool = ICurvePool(_pool);
-        pool.claim_admin_fees();
-    }
-
-    /// @notice Return the USD value of given Curve Lp, with 18 decimals of precision.
-    /// @param crvLp The ERC-20 Curve LP token to check the value.
+    /// @inheritdoc IBaseOracle
     function getPrice(address crvLp) external override returns (uint256) {
         (address pool, address[] memory tokens, uint256 virtualPrice) = _getPoolInfo(crvLp);
         _checkReentrant(pool, tokens.length);
@@ -119,12 +84,55 @@ contract CurveVolatileOracle is CurveBaseOracle {
         return (answer * Constants.CHAINLINK_PRICE_FEED_PRECISION) / Constants.PRICE_PRECISION;
     }
 
-    /// @dev Checks that value is within the range [lowerBound; upperBound],
-    /// @notice If the value is below the lowerBound, it reverts. Otherwise, it returns min(value, upperBound).
-    /// @param crvLp The curve LP token address
-    /// @param value Value to be checked and bounded
+    /**
+     * @notice Fetches the lower bound for the token-to-underlying exchange rate.
+     * @dev Used to protect against LP token / share price manipulation.
+     */
+    function getLowerBound() external view returns (uint256) {
+        return _lowerBound[msg.sender];
+    }
+
+    /**
+     * @notice Updates the bounds for the exchange rate value
+     * @param crvLp The Curve LP token address
+     * @param lowerBound The new lower bound (the upper bound is computed dynamically)
+     *                   from the lower bound
+     */
+    function setLimiter(address crvLp, uint256 lowerBound) external onlyOwner {
+        _setLimiter(crvLp, lowerBound);
+    }
+
+    /// @notice Internal implementation for setting the limiter
+    function _setLimiter(address crvLp, uint256 lowerBound) internal {
+        if (lowerBound == 0 || !_checkCurrentValueInBounds(crvLp, lowerBound, _upperBound(lowerBound))) {
+            revert Errors.INCORRECT_LIMITS();
+        }
+
+        _lowerBound[crvLp] = lowerBound;
+        emit NewLimiterParams(lowerBound, _upperBound(lowerBound));
+    }
+
+    /// @notice Checks if the current value is within the specified bounds
+    function _checkCurrentValueInBounds(
+        address crvLp,
+        uint256 lowerBound,
+        uint256 upperBound
+    ) internal view returns (bool) {
+        (, , uint256 virtualPrice) = _getPoolInfo(crvLp);
+        if (virtualPrice < lowerBound || virtualPrice > upperBound) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @notice Checks that value is within the range [lowerBound; upperBound],
+     * @dev If the value is below the lowerBound, it reverts. Otherwise, it returns min(value, upperBound).
+     * @param crvLp The curve LP token address
+     * @param value Value to be checked and bounded
+     */
     function _checkAndUpperBoundValue(address crvLp, uint256 value) internal view returns (uint256) {
-        uint256 lb = lowerBound[crvLp];
+        uint256 lb = _lowerBound[crvLp];
         if (value < lb) revert Errors.VALUE_OUT_OF_RANGE();
 
         uint256 uBound = _upperBound(lb);
@@ -132,8 +140,14 @@ contract CurveVolatileOracle is CurveBaseOracle {
         return (value > uBound) ? uBound : value;
     }
 
-    /// Computes the upper bound based on the provided lower bound
+    /// @notice Computes the upper bound based on the provided lower bound
     function _upperBound(uint256 lb) internal pure returns (uint256) {
         return (lb * (_PERCENTAGE_FACTOR + _RANGE_WIDTH)) / _PERCENTAGE_FACTOR;
+    }
+
+    /// @inheritdoc CurveBaseOracle
+    function _checkReentrant(address _pool, uint256) internal override {
+        ICurvePool pool = ICurvePool(_pool);
+        pool.claim_admin_fees();
     }
 }
