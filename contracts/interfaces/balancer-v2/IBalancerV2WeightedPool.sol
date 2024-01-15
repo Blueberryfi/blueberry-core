6ba4593f0aab7f07d192b550c8601329d89be81b
baileyspraggins <baileyspraggins@me.com>
2024-01-14 19:46:58 -0600

    Documentation and Visibility: Balancer Oracles

--- contracts/interfaces/balancer-v2/IBalancerV2WeightedPool.sol
+++ contracts/interfaces/balancer-v2/IBalancerV2WeightedPool.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.22;
 
-import "./IBalancerV2Pool.sol";
+import { IBalancerV2Pool } from "./IBalancerV2Pool.sol";
 
 interface IBalancerV2WeightedPool is IBalancerV2Pool {
     function getNormalizedWeights() external view returns (uint256[] memory);
