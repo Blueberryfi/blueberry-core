3254181101488efed86f7e52471360e23279d1d9
baileyspraggins <baileyspraggins@me.com>
2024-01-14 22:40:05 -0600

    Documentation and visibility: Uniswap Oracles

--- contracts/oracle/UniswapV3AdapterOracle.sol
+++ contracts/oracle/UniswapV3AdapterOracle.sol
@@ -22,53 +22,65 @@ import { UsingBaseOracle } from "./UsingBaseOracle.sol";
 
 import { IBaseOracle } from "../interfaces/IBaseOracle.sol";
 
-/// @author BlueberryProtocol
-/// @title Uniswap V3 Adapter Oracle
-/// @notice Oracle contract which provides price feeds of tokens from Uni V3 pool paired with stablecoins
+/**
+ * @author BlueberryProtocol
+ * @title Uniswap V3 Adapter Oracle
+ * @notice Oracle contract which provides price feeds of tokens from Uni V3 pool paired with stablecoins
+ */
 contract UniswapV3AdapterOracle is IBaseOracle, UsingBaseOracle, BaseAdapter {
     using SafeCast for uint256;
 
+    /*//////////////////////////////////////////////////////////////////////////
+                                      EVENTS
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice Emitted when a new stablecoin pool is set
     event SetPoolStable(address token, address pool);
 
+    /*//////////////////////////////////////////////////////////////////////////
+                                      STORAGE
+    //////////////////////////////////////////////////////////////////////////*/
+
     /// @dev Mapping from token address to Uni V3 pool of token/(USDT|USDC|DAI) pair
-    mapping(address => address) public stablePools;
+    mapping(address => address) private _stablePools;
 
     /*//////////////////////////////////////////////////////////////////////////
                                      CONSTRUCTOR
     //////////////////////////////////////////////////////////////////////////*/
 
-    constructor(IBaseOracle _base) UsingBaseOracle(_base) {}
+    constructor(IBaseOracle base) UsingBaseOracle(base) {}
 
     /*//////////////////////////////////////////////////////////////////////////
                                       FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*/
 
-    /// @notice Set stablecoin pools for multiple tokens
-    /// @param tokens list of tokens to set stablecoin pool references
-    /// @param pools list of reference pool addresses
+    /**
+     * @notice Set stablecoin pools for multiple tokens
+     * @param tokens list of tokens to set stablecoin pool references
+     * @param pools list of reference pool addresses
+     */
     function setStablePools(address[] calldata tokens, address[] calldata pools) external onlyOwner {
-        if (tokens.length != pools.length) revert Errors.INPUT_ARRAY_MISMATCH();
+        uint256 tokenLength = tokens.length;
+        if (tokenLength != pools.length) revert Errors.INPUT_ARRAY_MISMATCH();
 
-        for (uint256 i = 0; i < tokens.length; ++i) {
+        for (uint256 i = 0; i < tokenLength; ++i) {
             if (tokens[i] == address(0) || pools[i] == address(0)) revert Errors.ZERO_ADDRESS();
             if (tokens[i] != IUniswapV3Pool(pools[i]).token0() && tokens[i] != IUniswapV3Pool(pools[i]).token1()) {
                 revert Errors.NO_STABLEPOOL(pools[i]);
             }
 
-            stablePools[tokens[i]] = pools[i];
+            _stablePools[tokens[i]] = pools[i];
             emit SetPoolStable(tokens[i], pools[i]);
         }
     }
 
-    /// @notice Return USD price of given token, multiplied by 10**18.
-    /// @param token The vault token to get the price of.
-    /// @return price USD price of token in 18 decimals.
+    /// @inheritdoc IBaseOracle
     function getPrice(address token) external override returns (uint256) {
         /// Maximum cap of timeGap is 2 days(172,800), safe to convert
         uint32 secondsAgo = timeGaps[token].toUint32();
         if (secondsAgo == 0) revert Errors.NO_MEAN(token);
 
-        address stablePool = stablePools[token];
+        address stablePool = getStablePool(token);
         if (stablePool == address(0)) revert Errors.NO_STABLEPOOL(token);
 
         address poolToken0 = IUniswapV3Pool(stablePool).token0();
@@ -87,5 +99,14 @@ contract UniswapV3AdapterOracle is IBaseOracle, UsingBaseOracle, BaseAdapter {
         );
 
         return (quoteTokenAmountForStable * base.getPrice(stablecoin)) / 10 ** stableDecimals;
+    }
+
+    /**
+     * @notice Get the stable pool for a given token
+     * @param token The address of the token to get the stable pool for
+     * @return The address of the stable pool
+     */
+    function getStablePool(address token) public view returns (address) {
+        return _stablePools[token];
     }
 }
