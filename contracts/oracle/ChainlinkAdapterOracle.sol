5e8f929a8b70b6e8e93b458986ce9b229a25477a
baileyspraggins <baileyspraggins@me.com>
2024-01-14 20:45:39 -0600

    Documentation and Visibility: chainlink oracles

--- contracts/oracle/ChainlinkAdapterOracle.sol
+++ contracts/oracle/ChainlinkAdapterOracle.sol
@@ -22,10 +22,13 @@ import { IBaseOracle } from "../interfaces/IBaseOracle.sol";
 import { IFeedRegistry } from "../interfaces/chainlink/IFeedRegistry.sol";
 import { IWstETH } from "../interfaces/IWstETH.sol";
 
-/// @title ChainlinkAdapterOracle for L1 Chains
-/// @author BlueberryProtocol
-/// @notice This Oracle Adapter leverages Chainlink's decentralized price feeds to provide accurate price data.
-///         It also supports remapping of tokens to their canonical forms (e.g., WBTC to BTC).
+/**
+ * @title ChainlinkAdapterOracle
+ * @dev This Oracle Adapter is for L1 Chains
+ * @author BlueberryProtocol
+ * @notice This Oracle Adapter leverages Chainlink's decentralized price feeds to provide accurate price data.
+ *         It also supports remapping of tokens to their canonical forms (e.g., WBTC to BTC).
+ */
 contract ChainlinkAdapterOracle is IBaseOracle, BaseAdapter {
     using SafeCast for int256;
 
@@ -33,106 +36,127 @@ contract ChainlinkAdapterOracle is IBaseOracle, BaseAdapter {
                                       PUBLIC STORAGE 
     //////////////////////////////////////////////////////////////////////////*/
 
-    /// Chainlink feed registry for accessing price feeds.
+    /// @dev Chainlink feed registry for accessing price feeds.
     /// (source: https://github.com/smartcontractkit/chainlink/blob/develop/contracts/src/v0.8/Denominations.sol)
-    IFeedRegistry public registry;
-    /// Address representing USD in Chainlink's denominations.
-    address public constant USD = address(840);
+    IFeedRegistry private _registry;
 
     /// @dev Maps tokens to their canonical form for price querying.
     ///      For example, WETH may be remapped to ETH, WBTC to BTC, etc.
-    mapping(address => address) public remappedTokens;
+    mapping(address => address) private _remappedTokens;
+
+    /// @dev Address representing USD in Chainlink's denominations.
+    address private constant _USD = address(840);
 
     /// @dev WstETH address
-    address public constant WSTETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
+    address private constant _WSTETH = 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0;
 
     /// @dev ankrETH address
-    address public constant ANKRETH = 0xE95A203B1a91a908F9B9CE46459d101078c2c3cb;
+    address private constant _ANKRETH = 0xE95A203B1a91a908F9B9CE46459d101078c2c3cb;
 
     /*//////////////////////////////////////////////////////////////////////////
                                      EVENTS
     //////////////////////////////////////////////////////////////////////////*/
 
-    /// @notice Emitted when the Chainlink feed registry used by the adapter is updated.
-    /// @param registry The new Chainlink feed registry address.
+    /**
+     * @notice Emitted when the Chainlink feed registry used by the adapter is updated.
+     * @param registry The address of the updated registry.
+     */
     event SetRegistry(address registry);
 
-    /// @notice Emitted when a token is remapped to its canonical form.
-    /// @param token The original token address that's being remapped.
-    /// @param remappedToken The canonical form of the token to which the original is remapped.
+    /**
+     * @notice Emitted when a token is remapped to its canonical form.
+     * @param token The original token address that's being remapped.
+     * @param remappedToken The canonical form of the token to which the original is remapped.
+     */
     event SetTokenRemapping(address indexed token, address indexed remappedToken);
 
     /*//////////////////////////////////////////////////////////////////////////
                                      CONSTRUCTOR
     //////////////////////////////////////////////////////////////////////////*/
 
-    /// @param registry_ Chainlink feed registry address.
-    constructor(IFeedRegistry registry_) {
-        if (address(registry_) == address(0)) revert Errors.ZERO_ADDRESS();
+    /// @param registry Chainlink feed registry address.
+    constructor(IFeedRegistry registry) {
+        if (address(registry) == address(0)) revert Errors.ZERO_ADDRESS();
 
-        registry = registry_;
+        _registry = registry;
     }
 
     /*//////////////////////////////////////////////////////////////////////////
                                       FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*/
 
-    /// @notice Updates the Chainlink feed registry used by this adapter.
-    /// @param registry_ The new Chainlink feed registry address.
-    /// @dev Can only be called by the contract owner.
-    function setFeedRegistry(IFeedRegistry registry_) external onlyOwner {
-        if (address(registry_) == address(0)) revert Errors.ZERO_ADDRESS();
-        registry = registry_;
-        emit SetRegistry(address(registry_));
+    /**
+     * @notice Updates the Chainlink feed registry used by this adapter.
+     * @param registry The new Chainlink feed registry address.
+     */
+    function setFeedRegistry(IFeedRegistry registry) external onlyOwner {
+        if (address(registry) == address(0)) revert Errors.ZERO_ADDRESS();
+        registry = registry;
+        emit SetRegistry(address(registry));
     }
 
-    /// @notice Maps a list of tokens to their canonical form for price queries.
-    /// @param tokens_ The list of tokens to be remapped.
-    /// @param remappedTokens_ The list of tokens to remap to.
-    /// @dev Both arrays should have the same length. Can only be called by the contract owner.
-    function setTokenRemappings(address[] calldata tokens_, address[] calldata remappedTokens_) external onlyOwner {
-        if (remappedTokens_.length != tokens_.length) revert Errors.INPUT_ARRAY_MISMATCH();
-        for (uint256 i = 0; i < tokens_.length; ++i) {
-            if (tokens_[i] == address(0)) revert Errors.ZERO_ADDRESS();
+    /**
+     * @notice Maps a list of tokens to their canonical form for price queries.
+     * @param tokens The list of tokens to be remapped.
+     * @param remappedTokens The list of tokens to remap to.
+     */
+    function setTokenRemappings(address[] calldata tokens, address[] calldata remappedTokens) external onlyOwner {
+        uint256 tokensLength = tokens.length;
+        if (tokensLength != remappedTokens.length) revert Errors.INPUT_ARRAY_MISMATCH();
+        for (uint256 i = 0; i < tokensLength; ++i) {
+            if (tokens[i] == address(0)) revert Errors.ZERO_ADDRESS();
 
-            remappedTokens[tokens_[i]] = remappedTokens_[i];
-            emit SetTokenRemapping(tokens_[i], remappedTokens_[i]);
+            _remappedTokens[tokens[i]] = remappedTokens[i];
+            emit SetTokenRemapping(tokens[i], remappedTokens[i]);
         }
     }
 
-    /// @notice Fetches the USD price of the specified token from Chainlink's feed.
-    ///         If the token has been remapped, it uses the canonical form for price querying.
-    /// @param token_ Address of the token for which to fetch the price.
-    /// @return price The USD price of the token, represented with 18 decimals.
-    function getPrice(address token_) external view override returns (uint256) {
+    /// @inheritdoc IBaseOracle
+    function getPrice(address token) external view override returns (uint256) {
         /// remap token if possible
-        address token = remappedTokens[token_];
-        if (token == address(0)) token = token_;
+        address remappedToken = _remappedTokens[token];
+        if (remappedToken == address(0)) remappedToken = token;
 
-        uint256 maxDelayTime = timeGaps[token];
-        if (maxDelayTime == 0) revert Errors.NO_MAX_DELAY(token_);
+        uint256 maxDelayTime = timeGaps[remappedToken];
+        if (maxDelayTime == 0) revert Errors.NO_MAX_DELAY(token);
+
+        IFeedRegistry registry = getFeedRegistry();
 
         /// Get token-USD price
-        uint256 decimals = registry.decimals(token, USD);
+        uint256 decimals = registry.decimals(remappedToken, _USD);
         (uint80 roundID, int256 answer, , uint256 updatedAt, uint80 answeredInRound) = registry.latestRoundData(
-            token,
-            USD
+            remappedToken,
+            _USD
         );
 
-        if (updatedAt < block.timestamp - maxDelayTime) revert Errors.PRICE_OUTDATED(token_);
-        if (answer <= 0) revert Errors.PRICE_NEGATIVE(token_);
-        if (answeredInRound < roundID) revert Errors.PRICE_OUTDATED(token_);
+        if (updatedAt < block.timestamp - maxDelayTime) revert Errors.PRICE_OUTDATED(token);
+        if (answer <= 0) revert Errors.PRICE_NEGATIVE(token);
+        if (answeredInRound < roundID) revert Errors.PRICE_OUTDATED(token);
 
-        if (token_ == WSTETH) {
+        if (token == _WSTETH) {
             return
-                ((answer.toUint256() * Constants.PRICE_PRECISION) * IWstETH(WSTETH).stEthPerToken()) /
+                ((answer.toUint256() * Constants.PRICE_PRECISION) * IWstETH(_WSTETH).stEthPerToken()) /
                 10 ** (18 + decimals);
-        } else if (token_ == ANKRETH) {
+        } else if (token == _ANKRETH) {
             return
                 ((answer.toUint256() * Constants.PRICE_PRECISION) *
-                    IAnkrETH(ANKRETH).sharesToBonds(Constants.PRICE_PRECISION)) / 10 ** (18 + decimals);
+                    IAnkrETH(_ANKRETH).sharesToBonds(Constants.PRICE_PRECISION)) / 10 ** (18 + decimals);
         }
 
         return (answer.toUint256() * Constants.PRICE_PRECISION) / 10 ** decimals;
+    }
+
+    /// @notice Returns the Chainlink feed registry used by this adapter.
+    function getFeedRegistry() public view returns (IFeedRegistry) {
+        return _registry;
+    }
+
+    /**
+     * @notice Returns the canonical form of the specified token, if it exists.
+     * @param token The token address to check.
+     * @return The canonical form of the token, if it exists.
+     */
+    function getTokenRemapping(address token) public view returns (address) {
+        return _remappedTokens[token];
     }
 }
--- contracts/oracle/ChainlinkAdapterOracleL2.sol
+++ contracts/oracle/ChainlinkAdapterOracleL2.sol
@@ -21,10 +21,12 @@ import { BaseAdapter } from "./BaseAdapter.sol";
 import { IBaseOracle } from "../interfaces/IBaseOracle.sol";
 import { ISequencerUptimeFeed } from "../interfaces/chainlink/ISequencerUptimeFeed.sol";
 
-/// @title Chainlink Adapter Oracle for L2 chains including Arb, Optimism, etc.
-/// @author BlueberryProtocol
-/// @notice This contract integrates Chainlink's Oracle to fetch price data on Layer 2 networks.
-///         It also monitors the uptime status of the L2 sequencer.
+/**
+ * @title ChainlinkAdapterOracleL2
+ * @author BlueberryProtocol
+ * @notice This contract integrates Chainlink's Oracle to fetch price data on Layer 2 networks.
+ *         It also monitors the uptime status of the L2 sequencer.
+ */
 contract ChainlinkAdapterOracleL2 is IBaseOracle, BaseAdapter {
     using SafeCast for int256;
 
@@ -32,80 +34,93 @@ contract ChainlinkAdapterOracleL2 is IBaseOracle, BaseAdapter {
                                       PUBLIC STORAGE 
     //////////////////////////////////////////////////////////////////////////*/
 
-    /// Reference to the sequencer uptime feed (used to monitor L2 chain status).
-    ISequencerUptimeFeed public sequencerUptimeFeed;
+    /// @dev Reference to the sequencer uptime feed (used to monitor L2 chain status).
+    ISequencerUptimeFeed private _sequencerUptimeFeed;
 
     /// @dev A mapping from a token address to its associated Chainlink price feed.
-    mapping(address => address) public priceFeeds;
+    mapping(address => address) private _priceFeeds;
 
     /*//////////////////////////////////////////////////////////////////////////
                                      EVENTS
     //////////////////////////////////////////////////////////////////////////*/
 
-    /// @notice Emitted when the registry is updated.
-    /// @param registry The address of the updated registry.
+    /**
+     * @notice Emitted when the Chainlink feed registry used by the adapter is updated.
+     * @param registry The address of the updated registry.
+     */
     event SetRegistry(address registry);
 
-    /// @notice Emitted when the L2 sequencer uptime feed registry source is updated.
-    /// @param registry The address of the updated L2 sequencer uptime feed registry.
+    /**
+     * @notice Emitted when the L2 sequencer uptime feed registry source is updated.
+     * @param registry The address of the updated L2 sequencer uptime feed registry.
+     */
     event SetSequencerUptimeFeed(address registry);
 
-    /// @notice Emitted when a new price feed for a token is set or updated.
-    /// @param token The address of the token for which the price feed is set or updated.
-    /// @param priceFeed The address of the Chainlink price feed for the token.
+    /**
+     * @notice Emitted when a new price feed for a token is set or updated.
+     * @param token The address of the token for which the price feed is set or updated.
+     * @param priceFeed The address of the Chainlink price feed for the token.
+     */
     event SetTokenPriceFeed(address indexed token, address indexed priceFeed);
 
     /*//////////////////////////////////////////////////////////////////////////
                                      CONSTRUCTOR
     //////////////////////////////////////////////////////////////////////////*/
 
-    /// @notice Constructs the ChainlinkAdapterOracleL2 and sets the L2 sequencer uptime feed.
-    /// @param sequencerUptimeFeed_ The Chainlink L2 sequencer uptime feed source.
-    constructor(ISequencerUptimeFeed sequencerUptimeFeed_) {
-        if (address(sequencerUptimeFeed_) == address(0)) {
+    /**
+     * @notice Constructs the ChainlinkAdapterOracleL2 and sets the L2 sequencer uptime feed.
+     * @param sequencerUptimeFeed The Chainlink L2 sequencer uptime feed source.
+     */
+    constructor(ISequencerUptimeFeed sequencerUptimeFeed) {
+        if (address(sequencerUptimeFeed) == address(0)) {
             revert Errors.ZERO_ADDRESS();
         }
 
-        sequencerUptimeFeed = sequencerUptimeFeed_;
+        _sequencerUptimeFeed = sequencerUptimeFeed;
     }
 
     /*//////////////////////////////////////////////////////////////////////////
                                       FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*/
 
-    /// @notice Sets the Chainlink L2 sequencer uptime feed registry source.
-    /// @param sequencerUptimeFeed_ Chainlink L2 sequencer uptime feed source.
-    function setSequencerUptimeFeed(ISequencerUptimeFeed sequencerUptimeFeed_) external onlyOwner {
-        if (address(sequencerUptimeFeed_) == address(0)) {
+    /**
+     * @notice Sets the Chainlink L2 sequencer uptime feed registry source.
+     * @param sequencerUptimeFeed The Chainlink L2 sequencer uptime feed source.
+     */
+    function setSequencerUptimeFeed(ISequencerUptimeFeed sequencerUptimeFeed) external onlyOwner {
+        if (address(sequencerUptimeFeed) == address(0)) {
             revert Errors.ZERO_ADDRESS();
         }
 
-        sequencerUptimeFeed = sequencerUptimeFeed_;
-        emit SetSequencerUptimeFeed(address(sequencerUptimeFeed_));
+        _sequencerUptimeFeed = sequencerUptimeFeed;
+        emit SetSequencerUptimeFeed(address(sequencerUptimeFeed));
     }
 
-    /// @notice Sets the price feeds for specified tokens.
-    /// @param tokens_ List of tokens for which the price feeds are being set.
-    /// @param priceFeeds_ Corresponding list of Chainlink price feeds.
-    function setPriceFeeds(address[] calldata tokens_, address[] calldata priceFeeds_) external onlyOwner {
-        if (tokens_.length != priceFeeds_.length) revert Errors.INPUT_ARRAY_MISMATCH();
-        for (uint256 i = 0; i < tokens_.length; ++i) {
-            if (tokens_[i] == address(0)) revert Errors.ZERO_ADDRESS();
-            if (priceFeeds_[i] == address(0)) revert Errors.ZERO_ADDRESS();
-            priceFeeds[tokens_[i]] = priceFeeds_[i];
+    /**
+     * @notice Sets the price feeds for specified tokens.
+     * @param tokens List of tokens for which the price feeds are being set.
+     * @param priceFeeds Corresponding list of Chainlink price feeds.
+     */
+    function setPriceFeeds(address[] calldata tokens, address[] calldata priceFeeds) external onlyOwner {
+        uint256 tokensLength = tokens.length;
+        if (tokensLength != priceFeeds.length) revert Errors.INPUT_ARRAY_MISMATCH();
 
-            emit SetTokenPriceFeed(tokens_[i], priceFeeds_[i]);
+        for (uint256 i = 0; i < tokensLength; ++i) {
+            if (tokens[i] == address(0)) revert Errors.ZERO_ADDRESS();
+            if (priceFeeds[i] == address(0)) revert Errors.ZERO_ADDRESS();
+            _priceFeeds[tokens[i]] = priceFeeds[i];
+
+            emit SetTokenPriceFeed(tokens[i], priceFeeds[i]);
         }
     }
 
-    /// @notice Returns the USD price of the specified token. Price value is with 18 decimals.
-    /// @param token_ Token address to get the price of.
-    /// @return price USD price of the specified token.
-    /// @dev Fetches the price from the Chainlink price feed, checks sequencer status, and verifies price validity.
-    function getPrice(address token_) external view override returns (uint256) {
+    /// @inheritdoc IBaseOracle
+    function getPrice(address token) external view override returns (uint256) {
         /// 1. Check for the maximum acceptable delay time.
-        uint256 maxDelayTime = timeGaps[token_];
-        if (maxDelayTime == 0) revert Errors.NO_MAX_DELAY(token_);
+        uint256 maxDelayTime = timeGaps[token];
+        if (maxDelayTime == 0) revert Errors.NO_MAX_DELAY(token);
+
+        ISequencerUptimeFeed sequencerUptimeFeed = getSequencerUptimeFeed();
 
         /// 2. L2 sequencer status check (0 = up, 1 = down).
         (, int256 answer, uint256 startedAt, , ) = sequencerUptimeFeed.latestRoundData();
@@ -122,16 +137,26 @@ contract ChainlinkAdapterOracleL2 is IBaseOracle, BaseAdapter {
         }
 
         /// 3. Retrieve the price from the Chainlink feed.
-        address priceFeed = priceFeeds[token_];
+        address priceFeed = getPriceFeed(token);
         if (priceFeed == address(0)) revert Errors.ZERO_ADDRESS();
 
         /// Get token-USD price
         (uint80 roundID, int256 price, , uint256 updatedAt, uint80 answeredInRound) = AggregatorV3Interface(priceFeed)
             .latestRoundData();
-        if (updatedAt < block.timestamp - maxDelayTime) revert Errors.PRICE_OUTDATED(token_);
-        if (price <= 0) revert Errors.PRICE_NEGATIVE(token_);
-        if (answeredInRound < roundID) revert Errors.PRICE_OUTDATED(token_);
+        if (updatedAt < block.timestamp - maxDelayTime) revert Errors.PRICE_OUTDATED(token);
+        if (price <= 0) revert Errors.PRICE_NEGATIVE(token);
+        if (answeredInRound < roundID) revert Errors.PRICE_OUTDATED(token);
 
         return (price.toUint256() * Constants.PRICE_PRECISION) / Constants.CHAINLINK_PRICE_FEED_PRECISION;
+    }
+
+    /// @notice Returns the Chainlink L2 sequencer uptime feed source
+    function getSequencerUptimeFeed() public view returns (ISequencerUptimeFeed) {
+        return _sequencerUptimeFeed;
+    }
+
+    /// @notice Returns the Chainlink price feed for the specified token.
+    function getPriceFeed(address token) public view returns (address) {
+        return _priceFeeds[token];
     }
 }
