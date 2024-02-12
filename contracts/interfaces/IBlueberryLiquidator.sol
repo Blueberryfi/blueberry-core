ead9f466b22ec77e8e3abdd07af81ae740c67dde
Bailey <baileyspraggins@me.com>
2024-02-11 21:22:12 -0600

    Remove blueberry market as flashloan option

--- contracts/interfaces/IBlueberryLiquidator.sol
+++ contracts/interfaces/IBlueberryLiquidator.sol
@@ -36,23 +36,4 @@ interface IBlueberryLiquidator is AutomationCompatibleInterface {
         address,
         bytes calldata data
     ) external returns (bool);
-
-
-    // /**
-    //  * @dev Receive a flash loan from the Blueberry Money Market.
-    //  * @param initiator The initiator of the loan.
-    //  * @param token The loan currency.
-    //  * @param amount The amount of tokens lent.
-    //  * @param fee The additional amount of tokens to repay.
-    //  * @param data Arbitrary data structure, intended to contain user-defined parameters.
-    //  * @return The keccak256 hash of "ERC3156FlashBorrower.onFlashLoan"
-    //  */
-    // function onFlashLoan(
-    //     address initiator,
-    //     address token,
-    //     uint256 amount,
-    //     uint256 fee,
-    //     bytes calldata data
-    // ) external returns (bytes32);
-
 }
