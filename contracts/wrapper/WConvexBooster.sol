59eff9dba6ca35bfedfc4894a10ad8a1d90eb13e
Bailey <baileyspraggins@me.com>
2024-04-15 20:36:32 -0500

    remove token Id validation checks

--- contracts/wrapper/WConvexBooster.sol
+++ contracts/wrapper/WConvexBooster.sol
@@ -12,6 +12,7 @@ pragma solidity 0.8.22;
 
 /* solhint-disable max-line-length */
 import { EnumerableSetUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableSetUpgradeable.sol";
+import { ERC1155Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 import { SafeERC20Upgradeable, IERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import { ReentrancyGuardUpgradeable } from "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import { Ownable2StepUpgradeable } from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
@@ -19,8 +20,6 @@ import { FixedPointMathLib } from "../libraries/FixedPointMathLib.sol";
 /* solhint-enable max-line-length */
 
 import "../utils/BlueberryErrors.sol" as Errors;
-
-import { BaseWrapper } from "./BaseWrapper.sol";
 
 import { IWConvexBooster } from "../interfaces/IWConvexBooster.sol";
 import { IERC20Wrapper } from "../interfaces/IERC20Wrapper.sol";
@@ -42,7 +41,12 @@ import { IRewarder } from "../interfaces/convex/IRewarder.sol";
  *      and do not generate yields. LP Tokens are identified by tokenIds
  *      encoded from lp token address.
  */
-contract WConvexBooster is IWConvexBooster, BaseWrapper, ReentrancyGuardUpgradeable, Ownable2StepUpgradeable {
+contract WConvexBooster is 
+    IWConvexBooster,
+    ERC1155Upgradeable,
+    ReentrancyGuardUpgradeable,
+    Ownable2StepUpgradeable
+{
     using EnumerableSetUpgradeable for EnumerableSetUpgradeable.AddressSet;
     using SafeERC20Upgradeable for IERC20Upgradeable;
     using FixedPointMathLib for uint256;
@@ -147,8 +151,6 @@ contract WConvexBooster is IWConvexBooster, BaseWrapper, ReentrancyGuardUpgradea
         /// cvx reward handle logic
         uint256 cvxRewardPerToken = IRewarder(ConvexRewarder).rewardPerToken();
         id = encodeId(pid, cvxRewardPerToken);
-
-        _validateTokenId(id);
 
         _mint(msg.sender, id, amount, "");
 
